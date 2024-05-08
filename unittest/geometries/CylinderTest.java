b2912b2c54844ec7b9d2aec5a81e1bf517041e84
ayeletfridman <ayfridma@g.jct.ac.il>
2024-05-08 21:59:30 +0300

    test for plane and cylinder classes

--- unittest/geometries/CylinderTest.java
+++ unittest/geometries/CylinderTest.java
@@ -1,7 +1,10 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
-
+import geometries.Cylinder;
+import primitives.Point;
+import primitives.Vector;
+import primitives.Ray;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -15,5 +18,23 @@ class CylinderTest {
      */
     @Test
     void getNormal() {
+        //Cylinder cylinder = new Cylinder(4, new Ray(new Point(0, 0, 1), new Vector(0, 0, 1)), 3);
+
+         ///============ Equivalence Partitions Tests ==============
+
+        //TC01:test that check if the point at the base of the cylinder
+        //asserEquals(new Vector(0, 0, 1), cylinder.getNormal(new Point(1, 2, 3)), "ERROR: the normal at the base is wrong value");
+        //TC02:test that check if the point at the top of the cylinder
+       // asserEquals(new Vector(0, 0, -1), cylinder.getNormal(new Point(1, 1, 0)), "ERROR: the normal at the top is wrong value");
+        //TC03:test that check if the point on the side of the cylinder
+       // asserEquals(new Vector(1, 0, 0), cylinder.getNormal(new Point(4, 0, 2)), "ERROR: the normal on the side is wrong value");
+
+        // =============== Boundary Values Tests ==================
+
+
+        //TC11: test that check if the point is the point center of the top
+        //asserEquals(new Vector(0, 0, 1), cylinder.getNormal(new Point(0, 0, 3)), "ERROR: the normal at the base is wrong value");
+        //TC12: test that check if the point is the point center of the base
+        //asserEquals(new Vector(0, 0, -1), cylinder.getNormal(new Point(0, 0, 0)), "ERROR: the normal at the top is wrong value");
     }
 }
--- unittest/geometries/PlaneTest.java
+++ unittest/geometries/PlaneTest.java
@@ -1,6 +1,10 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+import geometries.Plane;
 
 import static org.junit.jupiter.api.Assertions.*;
 /**
@@ -8,6 +12,15 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author gili and ayelet
  */
 class PlaneTest {
+
+    void testConstructor()
+    {
+        //TC01: The first and second points are connected
+        assertThrows(IllegalArgumentException.class, () -> new Plane(new Point(1, 1, 1), new Point(1, 1, 1), new Point(0, 1, 0)));
+        //TC02: The points are on the same line
+        assertThrows(IllegalArgumentException.class, () -> new Plane(new Point(1, 0, 0), new Point(2, 0, 0), new Point(3, 0,  0)));
+    }
+
     /**
      * Test method for getNormal function
      */
@@ -15,9 +28,23 @@ class PlaneTest {
     void getNormal() {
     }
     /**
-     * Test method for testGetNormal function
+     *  * Test method for {@link geometries.Plane#getNormal(primitives.Point)}.
      */
     @Test
     void testGetNormal() {
+        /// ============ Equivalence Partitions Tests ==============
+
+        Point[] pts = { new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0) };
+        Plane plan = new Plane(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0));
+        // ensure there are no exceptions
+        assertDoesNotThrow(() -> plan.getNormal(new Point(0, 0, 1)), "");
+        // generate the test result
+        Vector result = plan.getNormal(new Point(0, 0, 1));
+        // ensure |result| = 1
+        assertEquals(1, result.length(), 0.00000001, "Plane's normal is not a unit vector");
+        // ensure the result is orthogonal to all the edges
+        for (int i = 0; i < 2; ++i)
+            assertEquals(result.dotProduct(pts[i].subtract(pts[i == 0 ? 2 : i - 1])),0,0.000001,
+                    "Plane's normal is not orthogonal to one of the edges");
     }
 }
