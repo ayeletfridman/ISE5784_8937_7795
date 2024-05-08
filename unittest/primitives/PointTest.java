1cb098e8f02debaa363bb344557b413f92eb7d58
gilishechter <gishecht@g.jct.ac.il>
2024-05-08 20:39:18 +0300

    add java doc

---
+++ unittest/primitives/PointTest.java
@@ -0,0 +1,37 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * unit tests for point class
+ * @author gili and ayelet
+ */
+class PointTest {
+
+    /**
+     * Test method for add function
+     */
+    @Test
+     void add() {
+    }
+    /**
+     * Test method for subtract function
+     */
+    @Test
+    void subtract() {
+    }
+    /**
+     * Test method for distanceSquared function
+     */
+    @Test
+    void distanceSquared() {
+    }
+    /**
+     * Test method for distance function
+     */
+    @Test
+    void distance() {
+    }
+}
---
+++ unittest/primitives/VectorTest.java
@@ -0,0 +1,55 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * unit tests for vector class
+ * @author gili and ayelet
+ */
+class VectorTest {
+
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}
+     */
+    @Test
+    void add() {
+    }
+    /**
+     * Test method for scale function
+     */
+    @Test
+    void scale() {
+    }
+    /**
+     * Test method for dotProduct function
+     */
+    @Test
+    void dotProduct() {
+    }
+    /**
+     * Test method for crossProduct function
+     */
+    @Test
+    void crossProduct() {
+    }
+    /**
+     * Test method for lengthSquared function
+     */
+    @Test
+    void lengthSquared() {
+    }
+    /**
+     * Test method for length function
+     */
+    @Test
+    void length() {
+    }
+    /**
+     * Test method for normalize function
+     */
+    @Test
+    void normalize() {
+    }
+}
